3b5770510bb1fb39fda2101451318468ecb0f04c
Tzu-ping Chung <uranusjr@gmail.com>
2014-08-21 13:57:07 +0800

    Automate archive-export-zip-release

--- Tools/collect_info.py
+++ Tools/collect_info.py
@@ -3,11 +3,40 @@
 
 from __future__ import print_function
 import os
+import re
+import subprocess
+import shutil
 import zipfile
 from xml.etree import ElementTree
 
 
 OPENSSL = '/usr/bin/openssl'
+XCODEBUILD = '/usr/bin/xcodebuild'
+OSASCRIPT = '/usr/bin/osascript'
+
+BUILD_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Build')
+APP_PATH = os.path.join(BUILD_DIR, 'MacDown.app')
+ZIP_PATH = os.path.join(BUILD_DIR, 'MacDown.app.zip')
+
+NULL = os.devnull
+
+
+class CommandError(Exception):
+    pass
+
+
+def execute(*args):
+    proc = subprocess.Popen(
+        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+    )
+    stdout, stderr = proc.communicate()
+    if proc.returncode:
+        raise CommandError(
+            '"{cmd}" failed with error {code}.\n {output}'.format(
+                cmd=' '.join(args), code=proc.returncode, output=stderr
+            )
+        )
+    return stdout
 
 
 def print_value(key, value):
@@ -15,30 +44,63 @@ def print_value(key, value):
 
 
 def main(argv):
-    if len(argv) < 3:
+    if len(argv) < 2:
         name = os.path.basename(argv[0])
-        print('Usage: {name} update_archive private_key'.format(name=name))
+        print('Usage: {name} private_key'.format(name=name))
         return
 
-    archive_path = argv[1]
-    cert_path = argv[2]
-    print()
+    cert_path = argv[1]
 
+    print('Pre-build cleaning...')
+    if os.path.exists(BUILD_DIR):
+        try:
+            shutil.rmtree(BUILD_DIR)
+        except OSError:
+            pass
+        else:
+            os.mkdir(BUILD_DIR)
+    execute(
+        XCODEBUILD, 'clean', '-workspace', 'MacDown.xcworkspace',
+        '-scheme', 'MacDown',
+    )
+
+    print('Building application archive...')
+    output = execute(
+        XCODEBUILD, 'archive', '-workspace', 'MacDown.xcworkspace',
+        '-scheme', 'MacDown',
+    )
+    match = re.search(r'^\s*ARCHIVE_PATH: (.+)$', output, re.MULTILINE)
+    archive_path = match.group(1)
+    print('Exporting application bundle...')
+    execute(
+        XCODEBUILD, '-exportArchive', '-exportFormat', 'app',
+        '-archivePath', archive_path, '-exportPath', APP_PATH,
+    )
+
+    # Zip.
+    with zipfile.ZipFile(ZIP_PATH, 'w') as f:
+        for root, dirs, files in os.walk(APP_PATH):
+            for file in files:
+                f.write(os.path.join(root, file))
+
+    raw_input(
+        'Build finished. Press Return to display bundle information and '
+        'reveal ZIP archive.'
+    )
+
+    print()
     print('DSA signature:')
     command = (
-        '{openssl} dgst -sha1 -binary < "{archive}" | '
+        '{openssl} dgst -sha1 -binary < "{zip_path}" | '
         '{openssl} dgst -dss1 -sign "{cert}" | '
         '{openssl} enc -base64'
-    ).format(openssl=OPENSSL, archive=archive_path, cert=cert_path)
+    ).format(openssl=OPENSSL, zip_path=ZIP_PATH, cert=cert_path)
     os.system(command)
     print()
 
-    print_value('Archive size', os.path.getsize(archive_path))
+    print_value('Archive size', os.path.getsize(ZIP_PATH))
 
-    archive = zipfile.ZipFile(argv[1])
-    bundle_version = None
-    short_version = None
-    with archive.open('MacDown.app/Contents/Info.plist') as plist:
+    with open(os.path.join(APP_PATH, 'Contents', 'Info.plist')) as plist:
         tree = ElementTree.parse(plist)
         root = tree.getroot()
         for infodict in root:
@@ -54,6 +116,11 @@ def main(argv):
                     has_key = child.text
     print_value('Bundle version', bundle_version)
     print_value('Short version', short_version)
+
+    script = 'tell application "Finder" to reveal POSIX file "{zip}"'.format(
+        zip=os.path.abspath(ZIP_PATH)
+    )
+    execute(OSASCRIPT, '-e', script)
 
 
 if __name__ == '__main__':
