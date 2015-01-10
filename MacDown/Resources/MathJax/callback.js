f5b1e13395b388aaef647aece32361ccf782a69f
Tzu-ping Chung <uranusjr@gmail.com>
2015-01-10 15:11:06 +0800

    Only register MathJax callback when we should
    Fix #237.

--- MacDown/Resources/MathJax/callback.js
+++ MacDown/Resources/MathJax/callback.js
@@ -1,3 +1,5 @@
-MathJax.Hub.Register.StartupHook('End', function () {
-   MathJaxListener.invokeCallbackForKey_('End');
-});
+if (typeof MathJaxListener !== 'undefined') {
+  MathJax.Hub.Register.StartupHook('End', function () {
+    MathJaxListener.invokeCallbackForKey_('End');
+  });
+}
