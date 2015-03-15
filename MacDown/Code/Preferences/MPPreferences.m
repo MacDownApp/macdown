6187f5ac0953818f496103d509e22af579516277
Tzu-ping Chung <uranusjr@gmail.com>
2015-03-15 16:30:27 +0800

    Add docstrings to clarify pref initialization usages

--- MacDown/Code/Preferences/MPPreferences.m
+++ MacDown/Code/Preferences/MPPreferences.m
@@ -155,29 +155,55 @@ static NSString * const kMPDefaultHtmlStyleName = @"GitHub2";
         [defaults removeObjectForKey:key];
 }
 
+/** Load app-default preferences on first launch.
+ *
+ * Preferences that need to be initialized manually are put here, and will be
+ * applied when the user launches MacDown the first time.
+ *
+ * Avoid putting preferences that doe not need initialization here. E.g. a
+ * boolean preference defaults to `NO` implicitly (because `nil.booleanValue` is
+ * `NO` in Objective-C), thus does not need initialization.
+ *
+ * Note that since this is called only when the user launches the app the first
+ * time, new preferences that breaks backward compatibility should NOT be put
+ * here. An example would be adding a boolean config to turn OFF an existing
+ * functionality. If you add the defualt-loading code here, existing users
+ * upgrading from an old version will not have this method invoked, thus
+ * effecting app behavior.
+ *
+ * @see -loadDefaultUserDefaults
+ */
 - (void)loadDefaultPreferences
 {
     self.extensionIntraEmphasis = YES;
     self.extensionTables = YES;
     self.extensionFencedCode = YES;
     self.extensionFootnotes = YES;
-    self.editorBaseFontInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-        kMPDefaultEditorFontName, kMPDefaultEditorFontNameKey,
-        @(kMPDefaultEditorFontPointSize), kMPDefaultEditorFontPointSizeKey,
-    nil];
+    self.editorBaseFontInfo = @{
+        kMPDefaultEditorFontNameKey: kMPDefaultEditorFontName,
+        kMPDefaultEditorFontPointSizeKey: @(kMPDefaultEditorFontPointSize),
+    };
     self.editorStyleName = kMPDefaultEditorThemeName;
     self.editorHorizontalInset = kMPDefaultEditorHorizontalInset;
     self.editorVerticalInset = kMPDefaultEditorVerticalInset;
     self.editorLineSpacing = kMPDefaultEditorLineSpacing;
     self.editorSyncScrolling = kMPDefaultEditorSyncScrolling;
-    self.editorOnRight = NO;
-    self.editorShowWordCount = NO;
-    self.editorWordCountType = 0;
     self.htmlStyleName = kMPDefaultHtmlStyleName;
     self.htmlDefaultDirectoryUrl = [NSURL fileURLWithPath:NSHomeDirectory()
                                               isDirectory:YES];
 }
 
+/** Load default preferences when the app launches.
+ *
+ * Preferences that need to be initialized manually are put here, and will be
+ * applied when the user launches MacDown.
+ *
+ * This differs from -loadDefaultPreferences in that it is invoked *every time*
+ * MacDown is launched, making it suitable to perform backward-compatibility
+ * checks.
+ *
+ * @see -loadDefaultPreferences
+ */
 - (void)loadDefaultUserDefaults
 {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
