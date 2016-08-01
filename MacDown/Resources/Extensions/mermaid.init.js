6ac16ee0d7a1774694e431d36f5f7b52f24c3dba
西关 <louting.t@alibaba-inc.com>
2016-08-01 11:16:48 +0800

    refactor graphviz & mermaid init code

--- MacDown/Resources/Extensions/mermaid.init.js
+++ MacDown/Resources/Extensions/mermaid.init.js
@@ -10,19 +10,27 @@
     }
   });
 
-  var domAll = document.querySelectorAll(".language-mermaid");
-  for (var i = 0; i < domAll.length; i++) {
+  var init = function() {
+    var domAll = document.querySelectorAll(".language-mermaid");
+    for (var i = 0; i < domAll.length; i++) {
     var dom = domAll[i];
     var graphSource = dom.innerText || dom.textContent;
-
+ 
     dom = dom.parentElement;
     if (dom.tagName === "PRE") {
       dom = dom.parentElement;
     }
-
+ 
     var insertSvg = function(svgCode, bindFunctions){
       dom.innerHTML = svgCode;
     };
     var graph = mermaidAPI.render('graphDiv' + i, graphSource, insertSvg)
+    }
+  };
+ 
+  if (typeof window.addEventListener != "undefined") {
+    window.addEventListener("load", init, false);
+  } else {
+    window.attachEvent("onload", init);
   }
 })();
