2a06d5bf8c4b3b96e6fcfb5b7c0467753857221f
Lawrence Forooghian <lawrence@forooghian.com>
2023-03-04 11:21:54 -0300

    Use contents of version.txt for CFBundleShortVersionString
    From what I understand of the documentation added in babd821, we can’t
    put things like "d" in this string. Also I don’t want all that stuff
    about counting commits.

--- Tools/utils.sh
+++ Tools/utils.sh
@@ -1,27 +1,10 @@
 # First, check for git in $PATH
 hash git 2>/dev/null || { echo >&2 "Git required, not installed.  Aborting build number update script."; exit 0; }
 
-# Use the latest tag for short version (expected tag format "vn[.n[.n]]")
-# or if there are no tags, we make up version 0.0.<commit count>
+# Use the contents of the version.txt file.
 function get_short_version() {
-    LATEST_TAG=$(git describe --tags --match 'v*' --abbrev=0 2>/dev/null) || LATEST_TAG="HEAD"
-    if [ $LATEST_TAG = "HEAD" ]; then
-        COMMIT_COUNT=$(git rev-list --count HEAD)
-        LATEST_TAG="0.0.$COMMIT_COUNT"
-        COMMIT_COUNT_SINCE_TAG=0
-    else
-        COMMIT_COUNT_SINCE_TAG=$(git rev-list --count ${LATEST_TAG}..)
-        LATEST_TAG=${LATEST_TAG##v} # Remove the "v" from the front of the tag
-    fi
-
-    if [ $COMMIT_COUNT_SINCE_TAG = 0 ]; then
-        SHORT_VERSION="$LATEST_TAG"
-    else
-        local tools_dir=$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")
-        local next_version=$(cat "$tools_dir/version.txt")
-        SHORT_VERSION="${next_version}d${COMMIT_COUNT_SINCE_TAG}"
-    fi
-    echo $SHORT_VERSION
+    local tools_dir=$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")
+    cat "$tools_dir/version.txt"
 }
 
 # Bundle version (commits-on-main[-until-branch "." commits-on-branch])
