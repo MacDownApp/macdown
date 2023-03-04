508990e05da57881bdf84986dc2045aa50415598
Lawrence Forooghian <lawrence@forooghian.com>
2023-03-04 10:21:08 -0300

    Replace master with main in utils.sh
    I don’t yet really know what this script is doing, but I don’t have a
    branch called master in my fork.

--- Tools/utils.sh
+++ Tools/utils.sh
@@ -29,24 +29,24 @@ function get_short_version() {
     echo $SHORT_VERSION
 }
 
-# Bundle version (commits-on-master[-until-branch "." commits-on-branch])
-# Assumes that two release branches will not diverge from the same commit on master.
+# Bundle version (commits-on-main[-until-branch "." commits-on-branch])
+# Assumes that two release branches will not diverge from the same commit on main.
 function get_bundle_version() {
-    if [ $(git rev-parse --abbrev-ref HEAD) = "master" ]; then
-        MASTER_COMMIT_COUNT=$(git rev-list --count HEAD)
+    if [ $(git rev-parse --abbrev-ref HEAD) = "main" ]; then
+        MAIN_COMMIT_COUNT=$(git rev-list --count HEAD)
         BRANCH_COMMIT_COUNT=0
-        BUNDLE_VERSION="$MASTER_COMMIT_COUNT"
+        BUNDLE_VERSION="$MAIN_COMMIT_COUNT"
     else
-        if [ $(git rev-list --count master..) = 0 ]; then   # The branch is attached to master. Just count master.
-            MASTER_COMMIT_COUNT=$(git rev-list --count HEAD)
+        if [ $(git rev-list --count main..) = 0 ]; then   # The branch is attached to main. Just count main.
+            MAIN_COMMIT_COUNT=$(git rev-list --count HEAD)
         else
-            MASTER_COMMIT_COUNT=$(git rev-list --count $(git rev-list master.. | tail -n 1)^)
+            MAIN_COMMIT_COUNT=$(git rev-list --count $(git rev-list main.. | tail -n 1)^)
         fi
-        BRANCH_COMMIT_COUNT=$(git rev-list --count master..)
+        BRANCH_COMMIT_COUNT=$(git rev-list --count main..)
         if [ $BRANCH_COMMIT_COUNT = 0 ]; then
-            BUNDLE_VERSION="$MASTER_COMMIT_COUNT"
+            BUNDLE_VERSION="$MAIN_COMMIT_COUNT"
         else
-            BUNDLE_VERSION="${MASTER_COMMIT_COUNT}.${BRANCH_COMMIT_COUNT}"
+            BUNDLE_VERSION="${MAIN_COMMIT_COUNT}.${BRANCH_COMMIT_COUNT}"
         fi
     fi
     echo $BUNDLE_VERSION
